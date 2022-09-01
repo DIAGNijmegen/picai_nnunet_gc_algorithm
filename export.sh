845f19007c3eb00c6818892cbda19dca1021d589
Joeran Bosma <joeran@bosma.co>
2022-09-01 14:33:09 +0200

    Update version

--- export.sh
+++ export.sh
@@ -2,4 +2,4 @@
 
 ./build.sh
 
-docker save picai_baseline_nnunet_processor | gzip -c > picai_baseline_nnunet_processor_1.0.tar.gz
+docker save picai_baseline_nnunet_processor | gzip -c > picai_baseline_nnunet_processor_2.1.tar.gz
